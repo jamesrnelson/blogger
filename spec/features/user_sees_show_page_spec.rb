6dac614fdf74902e431e2585d625702ff23c2f7d
JNelson <nelson.jimmy@gmail.com>
2018-03-23 12:29:10 -0600

    new test for linking to specific article

---
+++ spec/features/user_sees_show_page_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'user sees one article' do
+  describe 'they link from the articles index' do
+    it 'displays information for one article' do
+      Article.create!(title: 'Title 5', body: 'body 5')
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+    end
+  end
+end
