917e6c6938132cd8810d092ee293948fbce52811
JNelson <nelson.jimmy@gmail.com>
2018-03-23 13:59:31 -0600

    added css file

--- spec/features/user_sees_show_page_spec.rb
+++ spec/features/user_sees_show_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'user sees one article' do
   describe 'they link from the articles index' do
     it 'displays information for one article' do
-      Article.create!(title: 'Title 5', body: 'body 5')
+      article = Article.create!(title: 'Title 5', body: 'body 5')
 
       visit articles_path
 
